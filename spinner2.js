5e3e0117211f7a7860b0c2f6b1741a74d690ef91
RyanDeMesa <ryandemesa09@gmail.com>
2022-11-22 23:45:26 +0000

    refactored again so array wasnt as long and used while loop

--- spinner2.js
+++ spinner2.js
@@ -1,9 +1,11 @@
-const spinner = [`\r|    `, `\r/    `, `\r-    `, `\r\\    `, `\r|    `, `\r|    `, `\r/    `, `\r-    `, `\r\\    `, `\r|    `]
-let timer = 100
-for (const index of spinner) {
- setTimeout(() => {
-  process.stdout.write(index)
- }, timer)
- timer += 200
+const spinner = [`\r|    `, `\r/    `, `\r-    `, `\r\\    `, `\r|    `];
+let timer = 100;
+while (timer <= 1900) {
+ for (const index of spinner) {
+  setTimeout(() => {
+   process.stdout.write(index);
+  }, timer);
+  timer += 200;
+ }
 }
 setTimeout(() => console.log(), 2100);
